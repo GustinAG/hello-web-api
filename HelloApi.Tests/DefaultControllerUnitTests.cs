f7b3c7160ba761ca28d0656507b8c91a0b426bdb
Gustin AG <gustinn@gmail.com>
2019-12-02 17:01:00 +0100

    + first unit test case

---
+++ HelloApi.Tests/DefaultControllerUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using HelloApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloApi.Tests
+{
+    [TestClass]
+    public class DefaultControllerUnitTests
+    {
+        [TestMethod]
+        public void Get_ShouldReturnSomeContent()
+        {
+            // Arrange
+            string text;
+            using (var controller = new DefaultController())
+            {
+                // Act
+                text = controller.Get();
+            }
+
+            // Assert
+            text.Should().NotBeNullOrWhiteSpace("text should contain something");
+        }
+    }
+}
---
+++ HelloApi.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Hello API - Unit & Integration Tests")]
+
+[assembly: Guid("ca99da73-b2d8-45a2-af7d-93be3ffd8ea3")]
